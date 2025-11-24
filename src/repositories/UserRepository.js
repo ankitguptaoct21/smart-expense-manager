d34ae56ee359e910dc6799f64a1c4ddef10d9cb5
Ankit Gupta <ankitgupta.oct21@gmail.com>
2025-11-25 01:14:42 +0530

    Refactors: code for change password, signin and signup

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,10 +1,5 @@
 import { User } from "../models/index.js";
 
-const _findUserByEmail = async email => await User
-        .findOne({ email })
-        .select("+password")
-        .exec();
-
 const createUser = async user => {
     try {
         const newUser =  await User.create({
@@ -22,7 +17,7 @@ const createUser = async user => {
 
 const authenticateUser = async (email, password) => {
     try {
-        const user = await _findUserByEmail(email);
+        const user = await User.findByEmailWithPassword(email);
 
         // If user does not exist
         if (!user) {
@@ -47,7 +42,7 @@ const authenticateUser = async (email, password) => {
 
 const changePassword = async (email, newPassword) => {
     try {
-        const user = await _findUserByEmail(email);
+        const user = await User.findByEmailWithPassword(email);
         
         if (!user) {
             throw new Error(`User with email ${email} doesn't exist.`);
@@ -56,7 +51,7 @@ const changePassword = async (email, newPassword) => {
         user.password = newPassword;
         await user.save();
 
-        return await _findUserByEmail(email).toJSON();
+        return user.toJSON();
 
     } catch (error) {
         console.log(error);
